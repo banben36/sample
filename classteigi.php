c44be6624c29170f6fd69fc5aa8fd8c48efa706e
banben36 <70253072+banben36@users.noreply.github.com>
2020-09-08 20:29:58 +0900

    Create classteigi.php

---
+++ classteigi.php
@@ -0,0 +1,14 @@
+<?php
+class Mondai{
+  public $number;
+  public $literal;
+  public $seikai;
+  
+  public function __construct($number,$literal,$seikai){
+    $this->number=$number;
+    $this->literal=$literal;
+    $this->seikai=$seikai;
+  }
+
+}
+?>
